bd5a23bb91bbd1d30da7aba238e217d05a996d8c
Павел Ложкин <lozhkin.pa@gmail.com>
2023-03-28 04:24:08 +0400

    Исправления после теста ЯП

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -52,13 +52,13 @@ class Comment(models.Model):
         Post,
         on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name = 'Пост'
+        verbose_name='Пост'
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name = 'Автор'
+        verbose_name='Автор'
     )
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
